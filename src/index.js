bb42dfa37dd6bae45271289b8a2771865f2e9903
Leamsi Escribano <leamsi.esc@gmail.com>
2020-07-16 14:50:39 -0400

    Slow connection

--- src/index.js
+++ src/index.js
@@ -12,4 +12,33 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  // Start of code
+  const toyCollectionElement = document.getElementById('toy-collection');
+  const url = 'http://localhost:3000/toys';
+
+  fetch(url).then((response) => {
+    return response.json();
+  })
+  .then(dataArray => {
+    for (const data of dataArray) {
+      console.log(data);
+      addToy(data.image)
+    }
+  })
+
+  function addToy(src) {
+    console.log(src);
+    const toyElement = document.createElement('div');
+    const imageElement = document.createElement('img');
+
+    imageElement.src = src;
+    toyElement.classList.add('card');
+    toyElement.appendChild(imageElement);
+
+    toyCollectionElement.appendChild(toyElement);
+
+  }
+
+
 });

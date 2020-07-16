c29ecc158d5c59e883d2207528fdc3d1e7f69dc1
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-07-16 16:18:45 -0400

    what

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-let addToy = false;
-
+// let toyObject;
 document.addEventListener("DOMContentLoaded", () => {
+  const toyCollection = document.getElementById("toy-collection");
+  let addToy = false;
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +15,38 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
+  function fetchToys() {
+    fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(toyObject => toyObject.map(toy => renderToy(toy)));
+  }
+
+  function renderToy(toy) {
+    toyCard = document.createElement("div")
+    toyCard.className = "card"
+    toyCard.innerHTML = `
+      <h2>${toy.name}</h2><br>
+      <img src="${toy.image}"><br>
+      <p>${toy.likes}</p><br>
+      <button class="like-btn">Like<3</button>
+     
+    `
+    toyCollection.append(toyCard)
+
+    //last thing for here: 
+  }
+
+  console.log('all good')
+
+  fetchToys()  
 });
+
+
+
+// function iterateToys(toyArray){
+//   for (i = 0; i < toyObject.length ; i++){
+//     console.log(toyArray[i])
+//   };
+// };

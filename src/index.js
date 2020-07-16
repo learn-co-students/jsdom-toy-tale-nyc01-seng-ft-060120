19bebef13bfa61aa73debdbdf7872dabf83a21ec
alexpac27 <alexpac27@gmail.com>
2020-07-16 19:35:09 -0400

    DONE

--- src/index.js
+++ src/index.js
@@ -3,6 +3,54 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toysUrl = "http://localhost:3000/toys"
+  const toyCollection = document.getElementById('toy-collection')
+
+  
+
+  // FETCH REQUESTS
+
+  function fetchToys(){
+    fetch(toysUrl)
+    .then(response => response.json())
+    .then(toys => toys.map(toy => renderToy(toy)))
+  }
+
+  function postToy(){
+    const inputArray = document.querySelectorAll('input')
+    fetch(toysUrl, {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        "name": `${inputArray[0].value}`,
+        "image":`${inputArray[1].value}`,
+        "likes": 0
+      })
+    }) .then(response => response.json())
+       .then(toy => renderToy(toy));
+  }
+
+  function updateLikes(n, id){
+    fetch(`${toysUrl}/${id}`, {
+      method: "PATCH",
+      headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        "likes": parseInt(n,10)
+      })
+     })
+    
+  }
+
+
+  //EVENT LISTENERS
+
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,40 +60,44 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-  const toysUrl = "http://localhost:3000/toys"
-  const toyCollection = document.getElementById('toy-collection')
+  
+  toyFormContainer.addEventListener('submit',(e) => {
+    e.preventDefault();
+    postToy();
+  });
 
-  function fetchToys(){
-    fetch(toysUrl)
-    .then(response => response.json())
-    .then(toys => renderToys(toys))
-  }
+  toyCollection.addEventListener('click',(e) => {
+    if (e.target.className === 'like-btn'){
+      let likesP = e.target.parentNode.querySelector('p')
+      let toyId = e.target.parentNode.id
+      likesP.innerText = `${parseInt(likesP.innerText,10) +1}`+  ` ` + `Likes`
+      let newLikes = likesP.innerText.split(" ")[0]
+    
+      updateLikes(newLikes, toyId);
+    }
+  });
+ 
+  
 
-  function renderToys(toys){
-    for (let i = 0; i < toys.length; i++){
+  // FUNCTIONS
+
+  function renderToy(toy){
+  
       const divCard = document.createElement('div')
-      divCard.innerHTML = `<h2>${toys[i].name}</h2>
-      
-        <img src=${toys[i].image} class="toy-avatar" />
-        <p>${toys[i].likes} Likes </p>
+      divCard.className = "card"
+      divCard.id = `${toy.id}`
+      divCard.innerHTML = `<h2>${toy.name}</h2>
+        <img src=${toy.image} class="toy-avatar" />
+        <p>${toy.likes} Likes </p>
         <button class="like-btn">Like <3</button>`
       toyCollection.append(divCard)
-    }
   }
   
-    // "id": 1,
-    // "name": "Woody",
-    // "image": "http://www.pngmart.com/files/3/Toy-Story-Woody-PNG-Photos.png",
-    // "likes": 5
-  
-    // createToyCard()
-    // <div class="card">
-    //   <h2>Woody</h2>
-    //   <img src=toy_image_url class="toy-avatar" />
-    //   <p>4 Likes </p>
-    //   <button class="like-btn">Like <3</button>
-    // </div>
-  
-    fetchToys()
+
+  // EXECUTIONS 
+
+    fetchToys();
 });
 
+
+//link up specific toy with the id in the database

3219bc40cb0c47356dd45aca850f634ea2190f44
mstaso <mstaso1@gmail.com>
2020-07-16 15:40:10 -0400

    first two deliverables

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // Start of code
+  const toyForm = document.querySelector(".add-toy-form")
   const toyCollectionElement = document.getElementById('toy-collection');
   const url = 'http://localhost:3000/toys';
 
@@ -22,23 +23,59 @@ document.addEventListener("DOMContentLoaded", () => {
   })
   .then(dataArray => {
     for (const data of dataArray) {
-      console.log(data);
-      addToy(data.image)
+      getToys(data)
     }
   })
 
-  function addToy(src) {
-    console.log(src);
+  function getToys(data) {
     const toyElement = document.createElement('div');
     const imageElement = document.createElement('img');
+    const likesElement = document.createElement('p');
+    const toyNameElement = document.createElement('h2');
+    const btnElement = document.createElement('button');
 
-    imageElement.src = src;
+    toyNameElement.textContent = data.name
+    likesElement.textContent = data.likes
+    btnElement.textContent = "Likes ❤"
+
+    imageElement.src = data.image;
+    imageElement.classList.add('toy-avatar');
+    btnElement.classList.add('like-btn');
+
     toyElement.classList.add('card');
-    toyElement.appendChild(imageElement);
-
+    toyElement.append(imageElement, toyNameElement, likesElement, btnElement);
     toyCollectionElement.appendChild(toyElement);
+  }
+  
+  
 
+  function addToy(body){
+    fetch(url, {
+      method: 'post',
+      body: JSON.stringify(body)
+    }).then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      ChromeSamples.log('Created Toy:', data.html_url);
+    });
   }
 
+  function formValues(e){
+    e.preventDefault()
+    const name = toyForm.name.value
+    const image = toyForm.image.value
+    console.log(name, image)
+    addToy({
+      name: name,
+      likes: 10,
+      image: image,
+      id: 9
+    })
+
+    
+  }
+  
+  
+  toyForm.addEventListener("submit", formValues)
 
 });

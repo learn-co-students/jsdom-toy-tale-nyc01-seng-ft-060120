95450c9128e130a932a06048db17ac872e0e52d2
SudoRudo <r.guerreronyc@gmail.com>
2020-07-16 17:28:24 -0400

    need likes

--- src/index.js
+++ src/index.js
@@ -16,31 +16,58 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  function newToy(data){
+    fetch('http://localhost:3000/toys',{
+      method: "POST",
+      headers: {  
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data) 
+    })
+    .then(response => response.json())
+    .then(toy => renderToy(toy))
+  };
+
+
+  
+  toyFormContainer.addEventListener("submit", function(e){
+    e.preventDefault();
+    const toyNameInput = document.getElementsByTagName("input")[0]
+    const toyImgInput = document.getElementsByTagName("input")[1]
+
+    newToy({"name":`${toyNameInput.value}`, "image": `${toyImgInput.value}`});
+
+  });
+
+
+
+
 
   function fetchToys() {
     fetch('http://localhost:3000/toys')
     .then(response => response.json())
-    .then(toyObject => toyObject.map(toy => renderToy(toy)));
-  }
+    .then(toyObject => toyObject.map(toy => renderToy(toy)))
+  };
 
   function renderToy(toy) {
     toyCard = document.createElement("div")
     toyCard.className = "card"
     toyCard.innerHTML = `
       <h2>${toy.name}</h2><br>
-      <img src="${toy.image}"><br>
+      <img src="${toy.image}" class="toy-avatar" /><br>
       <p>${toy.likes}</p><br>
       <button class="like-btn">Like<3</button>
-     
     `
     toyCollection.append(toyCard)
 
     //last thing for here: 
-  }
+  };
 
   console.log('all good')
 
-  fetchToys()  
+  fetchToys();  
+  newToy({"name": "Rudy", "image":"https://www.gannett-cdn.com/presto/2018/08/25/USAT/15c97a8b-8874-435d-9e44-18eb5b26ad35-rrudy_stl_9_h.jpg?width=300&height=438&fit=crop&format=pjpg&auto=webp", "likes":8});
 });
 
 
